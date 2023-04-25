a0e909433e2dd424a79c1da7914ef1815d66c9fb
Heiner Nuñez <heiner23.0@gmail.com>
2023-04-25 00:02:46 -0500

    use inline function for amount and volume credits

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -18,8 +18,8 @@ function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -64,6 +64,13 @@ class PerformanceCalculator {
             default:
                 throw new Error(`unknown type: ${this.play.type}`);
         }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
